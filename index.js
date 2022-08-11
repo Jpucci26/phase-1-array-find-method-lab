1d3272c6286a67a35b499dcd60941ac186420fe7
Jake <jakepucci26@gmail.com>
2022-08-10 19:18:52 -0600

    commit tree 54d7c2645f9724ce94417f32976f821573c695d7

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function superbowlWin(record){ 
+
+    // function checkWin(element) {
+    //     return element.result == 'W'
+    // }
+
+    let winner = record.find(element => element.result == 'W')
+
+    if (winner == undefined) {
+        return undefined
+    }
+    else {
+        return winner.year
+    }
+}
+
